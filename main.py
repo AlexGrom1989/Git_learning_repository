55fcf56105c9fb3119a2a32382d17784b3896096
alex <fordro00@mail.ru>
2024-08-05 18:10:52 +0300

    Дошел до декораторов ура

--- main.py
+++ main.py
@@ -32,9 +32,151 @@ def main():
         Counter.send(i)
     Counter.close()
 '''
-            
+''' Исключения. Функция документирования .__doc__ 
+def problem_func(value: int):
+    "Это документация к функции. попробуй вызвать problem_func.__doc__"
+    print(problem_func.__doc__)
+    raise(Exception("We have big problems!"))
 def main():
-    pass
+    try:
+        problem_func(0)
+    except IOError as e:
+        print("error:", e)
+    except Exception as e:
+        print(e)
+    finally:
+        print(12)
 
+    print(dir(int)) # По приколу вывел все методы класса int
+'''
+''' Типы/Классы данных.
+def main():
+    kop = 1 + 10j #Комплексное число
+    if type(kop) is complex: print('complexxx')
+    if isinstance(kop, complex): print('xomplexx')
+    if type(kop) == type([]): print("wow")
+    else: print('i forgive u')
+'''
+''' Глубокое кипорование. deepcopy
+from copy import deepcopy
+def main():
+    a = [1, [2, 3]]
+    b = a.copy()
+    b[0] = 4
+    b[1][0] = 5
+    print(a, b) # Заметим, что вложеный список изменен и в а, и в b, несмотря на copy
+    b = deepcopy(a) # Глубокое копирование
+    b[1][0] = 6
+    print(a, b) # А сейчас все гуд
+'''
+''' Первоклассным объектам (например словарю) можно присваивать функции, модули, методы и исключения
+def main():
+    items = dict()
+    items['function'] = abs
+    import math
+    items['module'] = math
+    nums = [1, 2, 3]
+    items['append'] = nums.append
+
+    items['append'](4)
+    print(
+        nums,
+        items['module'].sqrt(16),
+        items['function'](-4)
+    )
+'''
+'''
+def main():
+    "Применение Первоклассных объектов и zip на практике"
+    line = "123, 45.0j, abc"
+    types = [int, complex, str]
+    box = [tp(value) for tp, value in zip(types, line.split(', '))]
+    print(box)
+'''
+''' Методы и их вызов.
+class Foo(object):
+    def instance_method(self): # self - ссылка на экземляр класса Foo
+        pass
+    @classmethod
+    def class_method(cls): # cls - ссылка на сам класс Foo
+        pass
+    @staticmethod
+    def static_method():
+        pass
+
+def main():
+    f = Foo()
+
+    f.instance_method() # 1
+
+    meth = f.instance_method # 2
+    meth()
+
+    meth = Foo.instance_method # 3
+    meth(f)
+    # Все три способы работают
+'''
+''' Запуск строки, как кода. exec. repr and eval.
+def main():
+    exec('print("Hello World!")')
+    a = [1, 2, 3, 4, 5]
+    # a = {'a': 1}
+    b = repr(a) # -> "[1, 2, 3, 4, 5]"" # b = list.__repr__(a)
+    c = eval(b) # -> [1, 2, 3, 4, 5] # c = b.__eval__()
+    print(a, b, c, eval('[[1, 2], 3,"ert"]'))
+'''
+
+''' Оценка истинности последовательности. all() and any()
+def main():
+    print(all([1, 'ee', 0])) # -> False
+    print(any([1, 'ee', 0])) # -> True
+    print(all([10, abs, sum])) # -> True
+    print(all([10, abs, None])) # -> False
+'''
+''' Магия аргументов функций. *args and **params. Удобно совмещать, когда делаешь декоратор под разные функции
+def func(n:int=1, *args, **kwargs) -> None:
+    print(n)
+    print(args)
+    print(params)
+    print('-'*10)
+
+def main():
+    func()
+    func(100, [1, 2, 3], 'bomb', {'k': 1}, noname=17)
+    func(noname_1 = '123', noname_2 = [2, 3, 4], noname_3='prikol')
+'''
+''' Декораторы. Два способа связывания
+# Будь внимателен, все не так очевидно
+# Посмотри на вывод, сначала сработают принты внешнего декоратора!
+# потому что связывание функции с декоратором, это то же самое, что
+# вызов самого декоратора с передачей в него функции, как в способе 2.
+def decor(vfunc):
+    def callf(*args, **kwargs):
+        print('Execute function', vfunc.__name__)
+        return vfunc(*args, **kwargs)
+    return callf
+
+    
+def external_decor(vfunc):
+    print(external_decor.__name__)
+    return vfunc
+    
+@external_decor    
+@decor # 1 способ
+def func():
+    print('func')
+
+def func_1(): # 2 способ
+    print('func_1')
+func_1 = external_decor(decor(func_1))
+
+def main():
+    func()
+    print()
+    func_1()
+
+# PS: Функция-декоратор, применяемая к классу, всегда должна возвращать 
+объект класса
+'''
 if __name__ == "__main__":
     main()
